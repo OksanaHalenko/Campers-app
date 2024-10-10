aa1923e188aa17f401de31b3c0903f0f664ff33e
OksanaHalenko <150601280+OksanaHalenko@users.noreply.github.com>
2024-10-10 12:28:01 +0200

    add loader

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,27 +1,31 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import HomePage from "../../pages/HomePage/HomePage";
-import CatalogPage from "../../pages/CatalogPage/CatalogPage";
-import CamperPage from "../../pages/CamperPage/CamperPage";
+
 import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage";
 import Features from "../Features/Features";
 import Reviews from "../Reviews/Reviews";
+import Loader from "../Loader/Loader";
+import { lazy, Suspense } from "react";
+
+const HomePage = lazy(() => import("../../pages/HomePage/HomePage.jsx"));
+const CatalogPage = lazy(() =>
+  import("../../pages/CatalogPage/CatalogPage.jsx")
+);
+const CamperPage = lazy(() => import("../../pages/CamperPage/CamperPage.jsx"));
 
 function App() {
   return (
-    <>
-      <div>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/campers" element={<CatalogPage />} />
-          <Route path="/campers/:id" element={<CamperPage />}>
-            <Route path="features" element={<Features />} />
-            <Route path="reviews" element={<Reviews />} />
-          </Route>
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </div>
-    </>
+    <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/campers" element={<CatalogPage />} />
+        <Route path="/campers/:id" element={<CamperPage />}>
+          <Route path="features" element={<Features />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Suspense>
   );
 }
 
