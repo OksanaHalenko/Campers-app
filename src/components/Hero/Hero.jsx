7c20fe651ad5b8948a4e2da3517a0273f6c0ce0d
OksanaHalenko <150601280+OksanaHalenko@users.noreply.github.com>
2024-10-11 20:44:43 +0200

    add hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,13 +1,24 @@
+import { Link } from "react-router-dom";
 import css from "./Hero.module.css";
 
 function Hero() {
   return (
     <div className={css.container}>
-      <h1 className={css.title}>Campers of your dreams</h1>
-      <p className={css.description}>
-        You can find everything you want in our catalog
-      </p>
-      <button className={css.btn}>View Now</button>
+      <section className={css.section}>
+        <div className={css.wrapper}>
+          <h1 className={css.title}>Campers of your dreams</h1>
+          <p className={css.description}>
+            You can find everything you want in our catalog
+          </p>
+          <Link
+            to="/campers"
+            className={css.btn}
+            aria-label="View the  campers catalog"
+          >
+            View Now
+          </Link>
+        </div>
+      </section>
     </div>
   );
 }
